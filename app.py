8d87380cd13e59cd485a37a4243da074fa2a6776
Török Péter <torokpe@gmail.com>
2024-11-26 14:23:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-a = input('Give me your name')
+a = st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, placeholder="Choose an option", disabled=False, label_visibility="visible")
 st.write(f"Hi {a}")
